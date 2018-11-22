6d7a27aa691723e482f3cd7efdb85a4021066160
Nadia Hamadou-Adamou <hamadoun@ens-epua.univ-savoie.fr>
2018-11-22 09:35:34 +0100

    Tp2 modifié

--- python/TP/TP2/Q2/Q2Emoticon.py
+++ python/TP/TP2/Q2/Q2Emoticon.py
@@ -9,7 +9,10 @@ import pygame
 import math
 
 class Emoticon:
-
+    def __init__(self,generalConfiguration):
+        # ajout des coordonnées de la tete du centre
+        self.headCenter_X=self.generalConfiguration.screen()/2
+        self.headCenter_Y=(self.generalConfiguration.emoticonSize/2)+(self.generalConfiguration.emoticonBorder)+(self.generalConfiguration.buttonHeight)
     # Setters
     def setGeneralConfiguration(self, generalConfiguration):
         self.generalConfiguration = generalConfiguration
@@ -17,3 +20,6 @@ class Emoticon:
     # Draws the emoticon    
     def draw(self):   
         pass
+# retourne les coordonnées dans le repère (o,x,y) du parametre position
+    def headToArea(self,position):
+        return [self.headCenter_X+position[0], self.headCenter_Y-position[1]]
